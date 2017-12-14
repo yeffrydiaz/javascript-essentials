d83ad8e51ea7d1ee3ff3ade1b54487ffe379d720
Yeffry Diaz <yeffrydiaz@gmail.com>
2017-12-13 22:00:05 -0500

    2nd Commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express'),
+	app     = express(),
+	axios   = require('axios');
+
+	app.use(express.static("public"));
+    app.set("view engine","ejs");
+    
+app.get('/', function (req, res) {
+ res.render('index');
+});
+
+
+
+//////////////////////////////////
+app.get('*', function (req, res) {
+ res.send('Lost');
+});
+
+var port = process.env.PORT || 8008;
+app.listen(port, process.env.IP, function () {
+  console.log('App listening on port 8008!');
+});
