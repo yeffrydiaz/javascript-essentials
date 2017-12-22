839ea48ffa52485accf6ca3bc16b968e1614b48a
Yeffry Diaz <yeffrydiaz@gmail.com>
2017-12-22 16:53:44 -0500

    adding functions

--- public/js/exercise.js
+++ public/js/exercise.js
@@ -1,5 +1,5 @@
 var doc              = document,
-	win              = window,
+	  win              = window,
     randomUserAPIUrl = 'https://randomuser.me/api/';
 
  createIsIntegerHtmlComp();
@@ -16,7 +16,7 @@ var doc              = document,
             createmapDoubleValuesHtmlComp();
 
 win.onload = function(){
-fetchRandomUser();  
+acallrandomUser();  
  findIndex(arr);
   countDown(3);
  setInterval(bitCoinPrice,5000);
@@ -68,7 +68,6 @@ function arrSum(arr){
   doc.querySelector("#arrSum")
 .innerHTML = '<h4>Array Elements Sum is: ' + sum +'</h4>';
 }
-
 
 /////////Advance Web Dev//////////////////////////////////////////////////////////////
 
@@ -223,10 +222,10 @@ Examples:
 function createfEachDoubleValuesHtmlComp(){
 var values = [1,2,3,4,5,6];
  var title = 'forEach Double Values';
-  var body = '<div class="boxContent">Values: '+values+'</div><div class="boxContent">Duplicated Values: <span id="forEachDoubleValues"></span></div>';  
+  var body = '<div class="boxContent">Values: '+values+'</div><div class="boxContent">Duplicated Values: <span id="forEachDoubleValues" class="strong"></span></div>';  
    comp(title,body);
   doc.querySelector("#forEachDoubleValues")
-.innerHTML = '<strong>'+doubleValues(values)+'</strong>';
+.innerHTML = doubleValues(values);
 }
 
 function doubleValues(arr){
@@ -249,10 +248,10 @@ Examples:
 function createEvenValuesValuesHtmlComp(){
 var values = [1,2,3,4,5,6,7,8,9,10];
  var title = 'Even Values';
-  var body = '<div class="boxContent">Values: '+values+'</div><div class="boxContent">Even Values: <span id="evenValues"></span></div>';  
+  var body = '<div class="boxContent">Values: '+values+'</div><div class="boxContent">Even Values: <span id="evenValues" class="strong"></span></div>';  
    comp(title,body);
  doc.querySelector("#evenValues")
-.innerHTML = '<strong>'+evenValues(values)+'</strong>';
+.innerHTML = evenValues(values);
 }
 function evenValues(arr){
  var evenval = [];
@@ -276,10 +275,10 @@ Examples:
 function createshowFirstAndLastHtmlComp(){
 var values = ['home','trip','javascript','html','4565'];
  var title = 'First and Last Characters';
-  var body = '<div class="boxContent">Values: '+values+'</div><div class="boxContent">First and Last: <span id="firstlast"></span></div>';  
+  var body = '<div class="boxContent">Values: '+values+'</div><div class="boxContent">First and Last: <span id="firstlast" class="strong"></span></div>';  
    comp(title,body);
  doc.querySelector("#firstlast")
-.innerHTML = '<strong>'+showFirstAndLast(values)+'</strong>';
+.innerHTML = showFirstAndLast(values);
 }
 function showFirstAndLast(arr){
  var lastAndFirst = [];
@@ -308,10 +307,10 @@ function createaddKeyAndValueHtmlComp(){
    var key = 'title';
  var value = 'instructor';
  var title = 'Add Key and Value';
-  var body = '<div class="boxContent">Values: '+JSON.stringify(arr)+' key: '+key+' , value: '+value+'</div><div class="boxContent">Key and Value Added: <span id="addKeyAndValue"></span></div>';  
+  var body = '<div class="boxContent">Values: '+JSON.stringify(arr)+' key: '+key+' , value: '+value+'</div><div class="boxContent">Key and Value Added: <span id="addKeyAndValue" class="strong"></span></div>';  
    comp(title,body);
  doc.querySelector("#addKeyAndValue")
-.innerHTML = '<strong>'+JSON.stringify(addKeyAndValue(arr,key,value))+'</strong>';
+.innerHTML = JSON.stringify(addKeyAndValue(arr,key,value));
 }
 function addKeyAndValue(arr,key,value){
 var arrAdded = [];
@@ -337,10 +336,10 @@ Examples:
 function createvowelCountHtmlComp(){
 var values = 'I Am awesome and so are you';
  var title = 'Vowels Counter';
-  var body = '<div class="boxContent">String: '+values+'</div><div class="boxContent">Counted Vowels: <span id="vowelCount"></span></div>';  
+  var body = '<div class="boxContent">String: '+values+'</div><div class="boxContent">Counted Vowels: <span id="vowelCount" class="strong"></span></div>';  
    comp(title,body);
  doc.querySelector("#vowelCount")
-.innerHTML = '<strong>'+JSON.stringify(vowelCount(values))+'</strong>';
+.innerHTML = JSON.stringify(vowelCount(values));
 }
 function vowelCount(str){
     var splitArr = str.split("");
@@ -392,10 +391,10 @@ Examples:
 function createmapDoubleValuesHtmlComp(){
 var values = [1,2,3,4,5,6,7,8,9,10];
  var title = 'Array map Doubled Values';
-  var body = '<div class="boxContent">String: '+values+'</div><div class="boxContent">Duplicated Values: <span id="mapDoubleValues"></span></div>';  
+  var body = '<div class="boxContent">String: '+values+'</div><div class="boxContent">Duplicated Values: <span id="mapDoubleValues" class="strong"></span></div>';  
    comp(title,body);
  doc.querySelector("#mapDoubleValues")
-.innerHTML = '<strong>'+mapDoubleValues(values)+'</strong>';
+.innerHTML = mapDoubleValues(values);
 }
 function mapDoubleValues(arr){
     return arr.map(function(numb){
@@ -798,36 +797,291 @@ function guessingGame(amount){
     };
 }
 
+//Arrow Functions/////////////////////////////////////////////////////////////////////////
+// function tripleAndFilter(arr){
+//   return arr.map(function(value){
+//     return value * 3;
+//   }).filter(function(value){
+//     return value % 5;
+//   })
+// }
+
+// 1
+let tripleAndFilter = arr => arr.map(val => val * 3).filter(val => val % 5 === 0);
+
+// function doubleOddNumbers(arr){
+//     return arr.filter(function(val){
+//         return val % 2 !== 0;
+//     }).map(function(val){
+//         return val *2;
+//     })
+// }
+
+// 2
+let doubleOddNumbersES5 = arr => arr.filter(val => val % 2 !== 0).map(val => val * 2 );
+
+// function mapFilterAndReduce(arr){
+//   return arr.map(function(val){
+//     return val.firstName
+//   }).filter(function(val){
+//     return val.length < 5;
+//   }).reduce(function(acc,next){
+//     acc[next] = next.length
+//     return acc;
+//   }, {})
+// }
+
+// 3
+let mapFilterAndReduce = arr => arr.map(val => val.firstName).filter(val => val.length < 5)
+  .reduce((acc,next) => {
+      acc[next] = next.length;
+      return acc;
+    }, {});
+
+// 4
+let createStudentObj = (firstName, lastName) => ({firstName:firstName, lastName,lastName});
+
+// var instructor = {
+//   firstName: "Colt",
+//   sayHi: function(){
+//     setTimeout(function(){
+//       console.log('Hello ' + this.firstName)
+//     },1000)
+//   }
+// }
+
+// 5
+var instructor = {
+  firstName: "Colt",
+  sayHi: function(){
+    setTimeout(() =>{
+      console.log('Hello ' + this.firstName)
+    }, 1000);
+  }
+};
+
+//Rest and Spread//////////////////////////////////////////////////////////////////////////
+/* 
+Write a function called smallestValue which accepts a variable number of parameters and returns the smallest parameters passed to the function.
+
+Examples:
+    smallestValue(4,1,12,0) // 0
+    smallestValue(5,4,1,121) // 1
+    smallestValue(4,2) // 2
+    smallestValue(99,12321,12.2) // 2
+*/
+//spread=============================================
+//ES5+
+let smallestValue = (...vals) => Math.min(...vals);
+//
+function smallestValue2(...vals){
+	return Math.min(...vals);      
+}
+
+/* 
+Write a function called placeInMiddle which accepts two parameters, an array and another array. This function should return the first array with all of the values in the second array placed in the middle of the first array.
+
+Examples:
+    placeInMiddle([1,2,6,7],[3,4,5]) // [1,2,3,4,5,6,7]
+    placeInMiddle([1],[3,4,5]) // [3,4,5,1]
+    placeInMiddle([1,6],[2,3,4,5]) // [1,2,3,4,5,6]
+    placeInMiddle([],[2,3,4,5]) // [2,3,4,5]
+*/
+// 
+function placeInMiddle(arr, vals){
+    arr.splice(Math.floor(arr.length/2), 0, ...vals);
+    return arr;
+}
+
+/* 
+Write a function called joinArrays which accepts a variable number of parameters (you can assume that each argument to this function will be an array) and returns an array of all of the parameters concatenated together
+
+Examples:
+
+    joinArrays([1],[2],[3]) // [1,2,3]
+    joinArrays([1],[2],[3],[1],[2],[3]) // [1,2,3,1,2,3]
+    joinArrays([1,2,3],[4,5,6],[7,8,9]) // [1,2,3,4,5,6,7,8,9]
+    joinArrays([1],[3],[0],[7]) // [1,3,0,7]
+
+*/
+//rest===========================
+function joinArrays(...args){
+  return args.reduce((acc, next) => acc.concat(next), [])
+}
+
+
+/* 
+// Write a function called sumEvenArgs which takes all of the parameters passed to a function and returns the sum of the even ones.
+
+Examples:
+    sumEvenArgs(1,2,3,4) // 6
+    sumEvenArgs(1,2,6) // 8
+    sumEvenArgs(1,2) // 2
+*/
+
+// rest
+function sumEvenArgs(...args){
+  return args.reduce((acc, next) => next % 2 === 0 ? acc += next : acc, 0)
+}
+
+/* 
+Write a function called flip which accepts a function and a value for the keyword this. Flip should return a new function that when invoked, will invoke the function passed to flip with the correct value of the keyword this and all of the parameters passed to the function REVERSED. HINT - if you pass more than two parameters to flip, those parameters should be included as parameters to the inner function when it is invoked. You will have to make use of closure!
+
+Examples:
+
+    function personSubtract(a,b,c){
+        return this.firstName + " subtracts " + (a-b-c);
+    }
+    
+    var person = {
+        firstName: 'Elie'
+    }
+    
+    var flipFn = flip(personSubtract, person);
+    flipFn(3,2,1) // "Elie subtracts -4"
+    
+    var flipFn2 = flip(personSubtract, person, 5,6);
+    flipFn(7,8). // "Elie subtracts -4"
+
+    flip(subtractFourNumbers,this,1)(2,3,4) // -2
+    flip(subtractFourNumbers,this,1,2)(3,4) // -2
+    flip(subtractFourNumbers,this,1,2,3)(4) // -2
+    flip(subtractFourNumbers,this,1,2,3,4)() // -2
+    flip(subtractFourNumbers,this)(1,2,3,4) // -2
+    flip(subtractFourNumbers,this,1,2,3)(4,5,6,7) // -2
+    flip(subtractFourNumbers,this)(1,2,3,4,5,6,7,8,9,10) // -2
+    flip(subtractFourNumbers,this,11,12,13,14,15)(1,2,3,4,5,6,7,8,9,10) // -22
+
+*/
+
+// rest
+function flip(fn, thisArg, ...outerArgs){
+  return function(...innerArgs){
+    let allArgs = outerArgs.concat(innerArgs).slice(0, fn.length)
+    return fn.apply(thisArg, allArgs.reverse());
+  }
+}
+
+/* 
+Write a function called bind which accepts a function and a value for the keyword this. Bind should return a new function that when invoked, will invoke the function passed to bind with the correct value of the keyword this. HINT - if you pass more than two parameters to bind, those parameters should be included as parameters to the inner function when it is invoked. You will have to make use of closure!
+
+Examples:
+
+    function firstNameFavoriteColor(favoriteColor){
+        return this.firstName + "'s favorite color is " + favoriteColor
+    }
+    
+    var person = {
+        firstName: 'Elie'
+    }
+    
+    var bindFn = bind(firstNameFavoriteColor, person);
+    bindFn('green') // "Elie's favorite color is green"
+    
+    var bindFn2 = bind(firstNameFavoriteColor, person, 'blue');
+    bindFn2('green') // "Elie's favorite color is blue" 
+    
+    function addFourNumbers(a,b,c,d){
+        return a+b+c+d;
+    }
+
+    bind(addFourNumbers,this,1)(2,3,4) // 10
+    bind(addFourNumbers,this,1,2)(3,4) // 10
+    bind(addFourNumbers,this,1,2,3)(4) // 10
+    bind(addFourNumbers,this,1,2,3,4)() // 10
+    bind(addFourNumbers,this)(1,2,3,4) // 10
+    bind(addFourNumbers,this)(1,2,3,4,5,6,7,8,9,10) // 10
+
+*/
+
+// rest + spread
+function bind(fn, thisArg, ...outerArgs){
+  return function(...innerArgs){
+    return fn.apply(thisArg, [...outerArgs, ...innerArgs])
+  };
+}
+
+//destructuring ====================================================
+/* 
+Write a function called displayStudentInfo which accepts an object and returns the string "Your full name is" concatenated with the value of the first key and a space and then the value of the last key. See if you can destructure this object inside of the function.
+
+Examples:
+    displayStudentInfo({first: 'Elie', last:'Schoppik'}) // 'Your full name is Elie Schoppik')
+*/
+
+function displayStudentInfo(obj){
+    var {first,last} = obj;
+    return 'Your full name is ' + first + ' ' + last;
+}
+
+/* 
+Write a function called printFullName which accepts an object and returns the string "Your full name is" concatenated with the value of the first key and a space and then the value of the last key. See if you can destructure this object DIRECTLY from the parameters. The output of the printFullName function should be the exact same as the displayStudentInfo function. 
+
+Examples:
+    printFullName({first: 'Elie', last:'Schoppik'}) // 'Your full name is Elie Schoppik'
+*/
+
+// you will have to pass in the correct parameters for this function!
+function printFullName({first,last}){
+  return `Your full name is ${first} ${last}`
+}
+
+/* 
+Write a function called createStudent which accepts as a parameter, a default parameter which is a destructured object with the key of likesES2015 and value of true, and key of likesJavaScript and value of true. 
+
+    If both the values of likesJavaScript and likesES2015 are true, the function should return the string 'The student likes JavaScript and ES2015'. 
+    If the value of likesES2015 is false the function should return the string 'The student likes JavaScript!'
+    If the value of likesJavaScript is false the function should return the string 'The student likesES2015!'
+    If both the value of likesJavaScript and likesES2015 are false, the function should return the string 'The student does not like much...'
+
+Examples:
+    createStudent() // 'The student likes JavaScript and ES2015')
+    createStudent({likesES2015:false}) // 'The student likes JavaScript!')
+    createStudent({likesJavaScript:false}) // 'The student likes ES2015!')
+    createStudent({likesJavaScript:false, likesES2015:false}) // 'The student does not like much...')
+*/
+
+// you will have to pass in the correct parameters for this function!
+function createStudent({likesJavaScript = true, likesES2015 = true} = {}){
+  var start = 'The student';
+  if(likesJavaScript && likesES2015){
+    start += ' likes JavaScript and ES2015';
+  } else if(likesJavaScript){
+    start += ' likes JavaScript!';
+  } else if(likesES2015){
+    start += ' likes ES2015!';
+  } else {
+    start += ' does not like much...';
+  }
+  return start;
+}
+
+/* 
+Write a function called reverseArray which accepts an array and returns the array with all values reversed. See if you can do this without creating a new array!
+
+Examples:
+    reverseArray([1,2,3,4,5]) // [5,4,3,2,1]
+    reverseArray([1,2]) // [2,1]
+    reverseArray([]) // []
+    reverseArray([1,2,3,4,5,6,7,8,9,10]) // [10,9,8,7,6,5,4,3,2,1
+*/
+
+function reverseArray(arr){
+  for(var i = 0; i < arr.length/2; i++){
+    [arr[i], arr[arr.length - 1 - i]] = [arr[arr.length - 1 - i], arr[i]]
+  }
+  return arr;
+}
+
+
+
+
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 //html component//////
 function comp(title,body){
  doc.querySelector(".container")
 	 .innerHTML += ('<article class="message is-info"><div class="message-header"><p>'+title+'</p></div>'+body+'</article>');
-}
-
-//acall///////////////
-function acall(obj) {	
- var asyn,method,url;
-  if(obj.data){
-	url = obj.url + '?' + obj.data;
-    }else{
-	  url = obj.url;
-     }
-     if(!obj.asyn){asyn=true;}
-      if(!obj.method){method="GET";}	
-       var xhr = new XMLHttpRequest();
-	    xhr.open(method,url,asyn);
-         xhr.send();
-          xhr.onreadystatechange = function(){
-	       if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200){  
-	        if(obj.dataType==="json"){
-			 obj.ok(JSON.parse(xhr.responseText));	    
-	     }else{
-		     obj.ok(xhr.responseText);
-	  }
-    }
-   };
 }
 
 //first word to Upper///
@@ -839,4 +1093,57 @@ return str
       return word[0].toUpperCase() + word.substr(1);
     })
  .join(' ');
+}
+
+/*
+function myAsyncFunction(url) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.onload = () => resolve(xhr.responseText);
+    xhr.onerror = () => reject(xhr.statusText);
+    xhr.send();
+  });
+}
+*/
+
+//acall///////////////
+function acall(obj) {	
+ let asyncr = obj.asyn || true,
+	   method = obj.method || "GET",
+	      url = obj.url,
+	 datasend;
+  if(obj.data){
+	  if(typeof obj.data === 'string'){
+	    datasend = obj.data;			  
+	    }else{
+		  var str = "";
+       for (var prop in obj.data) {
+         if(!obj.data.hasOwnProperty(prop)){continue;}
+          str += prop + "=" + obj.data[prop] + "&";	 
+         }
+		    datasend = str.slice(0,-1);		   	
+	      }		
+		  if(method === "GET"){
+		  url = url+"?"+datasend;  
+		  }	
+    }else{
+	   datasend = '';
+      }
+      let xhr = new XMLHttpRequest();	   
+	       xhr.onreadystatechange = function(){
+	        if (xhr.readyState === 4 && 
+							(xhr.status === 200 || xhr.status === 201) ){ 
+	         if(obj.dataType==="json"){
+			       obj.ok(JSON.parse(xhr.responseText));	    
+	         }else{
+		        obj.ok(xhr.responseText);
+	        }
+         }
+       };
+	  xhr.open(method,url,asyncr);
+    if(method==="POST" || method==="PUT"){
+		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	 }
+ xhr.send(datasend);
 }
