36bf2712a3378eb646a3b25a40dbf63c5821e952
Yeffry Diaz <yeffrydiaz@gmail.com>
2017-12-29 11:42:19 -0500

    adding script start

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-var express = require('express'),
-	app     = express(),
-	axios   = require('axios');
+const express = require('express'),
+	    app     = express(),
+	    axios   = require('axios'),
+      port    = process.env.PORT || 8008;
 
-	app.use(express.static("public"));
-    app.set("view engine","ejs");
+app.use(express.static("public"));
+app.set("view engine","ejs");
     
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
  res.render('index');
 });
 
 
 //////////////////////////////////
-app.get('*', function (req, res) {
+app.get('*', (req, res) => {
  res.send('Lost');
 });
 
-var port = process.env.PORT || 8008;
 app.listen(port, process.env.IP, function () {
   console.log('App listening on port 8008!');
 });
